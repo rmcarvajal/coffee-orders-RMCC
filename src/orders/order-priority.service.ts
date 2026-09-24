51defc217cbcb38ecb54f7eac0a904bad4cac7e3
rmcarvajal <darwyn089@gmail.com>
2026-09-24 17:57:20 -0500

    priority service done

--- src/orders/order-priority.service.ts
+++ src/orders/order-priority.service.ts
@@ -8,7 +8,32 @@ export class OrderPriorityService {
     status: string;
     quantity: number;
     priority: 'completed' | 'high' | 'medium' | 'normal';
+    message: string;
   } {
-    const priorityOrder
+    const priority =
+      order.status === 'ready'
+        ? 'completed'
+        : order.quantity >= 4
+          ? 'high'
+          : order.quantity <= 2
+            ? 'medium'
+            : 'normal';
+
+    const message =
+      priority === 'completed'
+        ? 'Order is ready'
+        : priority === 'high'
+          ? 'Prepare this order soon'
+          : priority === 'medium'
+            ? 'Order has medium priority'
+            : 'Order has normal priority';
+
+    return {
+      orderId: order.id,
+      status: order.status,
+      quantity: order.quantity,
+      priority,
+      message,
+    };
   }
 }
